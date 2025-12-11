f6bb0ef353a6181c239847c2df2daec2b5f19d72
Alumno3 <villamario7@digitechfp.com>
2025-12-11 18:01:24 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import * as path from 'node:path';
+import {ListPage} from './components/web/list-page/list-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'cards/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cards/list',
+    component: ListPage
+  },
+  {
+    path: 'cards/detail/:id',
+    component: DetailPage
+  },
+  {
+    path: '**',
+    redirectTo: 'cards/list',
+    pathMatch: 'full'
+  }
+];
