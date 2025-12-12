0269172bcf59a2b2c404f990052a27b24be6485c
Mario <villamario7@gmail.com>
2025-12-12 14:36:33 +0100

    update

---
+++ src/app/components/web/detail-page/detail-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-page',
+  imports: [],
+  templateUrl: './detail-page.html',
+  styleUrl: './detail-page.css',
+})
+export class DetailPage {
+
+}
