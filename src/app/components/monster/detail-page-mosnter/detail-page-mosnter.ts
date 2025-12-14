b3b201dba4a584c017c8ab0bb0abcabd6ecb488e
Mario <villamario7@gmail.com>
2025-12-14 02:52:05 +0100

    update

---
+++ src/app/components/monster/detail-page-mosnter/detail-page-mosnter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-page-mosnter',
+  imports: [],
+  templateUrl: './detail-page-mosnter.html',
+  styleUrl: './detail-page-mosnter.css',
+})
+export class DetailPageMosnter {
+
+}
---
+++ src/app/components/monster/list-page-mosnter/list-page-mosnter.ts
@@ -0,0 +1,53 @@
+import {Component, inject, signal, WritableSignal} from '@angular/core';
+import {DataService} from '../../../services/data-service';
+import { InfoMonster} from '../../../common/interfaces';
+import {NgbPagination} from '@ng-bootstrap/ng-bootstrap';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-list-page-mosnter',
+  imports: [
+    NgbPagination,
+  ],
+  templateUrl: './list-page-mosnter.html',
+  styleUrl: './list-page-mosnter.css',
+})
+export class ListPageMosnter {
+  private readonly dataService: DataService = inject(DataService);
+
+  monsterList: WritableSignal<InfoMonster[]>=signal<InfoMonster[]>([]);
+
+  loaded = false;
+  currentPage:number = 1;
+  readonly totalItems:number = 1306;
+
+  ngOnInit() {
+    this.loadMonster()
+  }
+
+  private loadMonster() {
+    this.loaded = false;
+    this.dataService.getDataMonster(this.currentPage).subscribe(
+      {
+        next: (data: InfoMonster[]) => {
+          this.monsterList.set(data);
+          this.loaded = true;
+        },
+        error: err => {
+          console.error(err);
+        }
+      }
+    )
+  }
+
+  pageChanged(event: number) {
+    if (event !== this.currentPage && event > 0) {
+      this.currentPage = event;
+      this.loadMonster();
+    }
+  }
+}
+
+
+
+
